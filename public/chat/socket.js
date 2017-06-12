6325f17c6975c3eedf9e1748dfa691620d5b9fa6
sguiderk <samir.guiderk@gmail.com>
2017-06-12 09:54:06 +0000

    Signed-off-by: sguiderk <samir.guiderk@gmail.com>

--- public/chat/socket.js
+++ public/chat/socket.js
@@ -4,52 +4,97 @@ var io = require('socket.io')(http);
 var port = process.env.PORT || 3004;
 const cookie = require('cookie');
 const cookieParser = require('cookie-parser');
+var mysql = require('mysql')
 
+var messages = []
+var isInitMessages = false
+var socketCount = 0
+
+// Define our db creds
+var db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password :'root',
+    database: 'zenddatabase'
+})
+ 
+// Log any errors connected to the db
+db.connect(function(err){
+    if (err) console.log(err)
+})
+ 
+// Define/initialize our global vars
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('petit_nouveau', function(pseudo) {
-    socket.pseudo = pseudo;
-      
-});
-
- io.on('sendSession', function(data) {
-
-  	    var username = data.sessionName;
-        var sessionId = data.sessionId;
- 
-        socket.username = username;
-        console.log(username);
-       
- });
-
- 
-
 
 io.on('connection', function(socket){
 
-	 
+  socketCount++  
+
+    socket.on('disconnect', function(socket){
+
+     socketCount-- 
+
+    });
 
 
-  socket.on('chat message', function(msg,username){
 
-  	 var username = cookie.parse(socket.handshake.headers.cookie).PHPSESSNAME;
+ socket.on('new messages', function(msg){
+       
+    var username = cookie.parse(socket.handshake.headers.cookie).PHPSESSNAME;
         
+         messages.push(msg)
+       
+         var values = [msg,username];
 
 
-    io.emit('chat message', {msg:msg,username:username});
+         db.query('INSERT INTO message (message,username) VALUES (?)', [values])
 
-  console.log(username);
+         io.emit('new messages',msg,username); 
+
+
+
+         //  console.log(messages);
+          // socket.emit('initial messages', messages)
+
+    }); 
  
-    
-  });
-  
 
-   
+
+// Check to see if initial query/Messages are set
+
+    if (! isInitMessages) {
+
+        // Initial app start, run db query
+
+        db.query('SELECT * FROM message')
+            .on('result', function(data){
+
+                // Push results onto the messages array
+
+                messages.push(data)
+            })
+            .on('end', function(){
+
+                // Only emit notes after query has been completed
+
+                socket.emit('initial messages', messages)
+            })
+ 
+        isInitMessages = true
+    } else {
+        // Initial notes already exist, send out
+        socket.emit('initial messages', messages) 
+    }
+
 
 });
+
+
+
 
 
 http.listen(port, function(){
